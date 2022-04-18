e9a93a61ba7239f6c14ac3408a2462a732efe897
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2022-04-18 12:09:16 -0400

    Update template.py

--- automatank/template.py
+++ automatank/template.py
@@ -1,14 +1,39 @@
 from godot import *
+from math import pi
 
 
 class TankController:
     def receive_initialise(self, controls):
         # Spend your skill points here.
+        '''
+        list of points
+        -------
+        controls.bullet_speed_points: points for bullet speed
+        controls.bullet_health_points: points for bullet health
+        controls.tank_speed_points: points for tank speed
+        controls.tank_health_points: points for tank health
+        controls.regen_points: points for health regen
+        '''
+        controls.tank_health_points = 2 # change the number of points or the stat as you wish!
+        controls.tank_name = "Your name here" # you can change the name here as well
+        controls.hex_colour = "#ffffff" # you can change the color here
 
-        controls.tank_name = "Your name here"
-        controls.hex_colour = "Your colour here"
-
-    def receive_update(self, controls, world):
+    def receive_update(self, controls, state):
+        '''
+        controls
+        ---------   
+        controls.shoot(angle): shoot at an angle (in radians)
+        controls.accelerate(direction): accelerate in a direction (Vector2)
+        ==================
+        state
+        ---------
+        state.position: returns a Vector2 of your position (down and right are the positive directions)
+        state.velocity: returns a Vector2 of your velocity
+        state.health: return your current health
+        state.max_health: your maximum health
+        state.tanks: list of all tanks
+        state.bullets: list of all bullets 
+        '''
         # Control the tank here.
-        pass # Remove the pass after you've written code.
-
+        controls.shoot(pi / 2) # shoot 90 degree down
+        # replace the code here by whatever you want
